dad9fe77e293a1d7d1af81fa7cb650f11ade9a9d
gnori-zon <108410527+gnori-zon@users.noreply.github.com>
2025-05-18 00:17:48 +0300

    #add #concurrency: tdd for concurrency module (#1)

---
+++ concurrency/concurrency.go
@@ -0,0 +1,25 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+
+type checkResult struct {
+	url       string
+	isSuccess bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	resultChannel := make(chan checkResult)
+	for _, url := range urls {
+		go func() {
+			isSuccess := wc(url)
+			resultChannel <- checkResult{url: url, isSuccess: isSuccess}
+		}()
+	}
+
+	results := make(map[string]bool)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.url] = result.isSuccess
+	}
+	return results
+}
