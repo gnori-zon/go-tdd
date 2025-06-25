da3c62aada4aeaa2bafd67570d66ed8bca635843
gnori-zon <konashenkos@gmail.com>
2025-06-26 00:36:21 +0300

    #add #generics: tdd for generics module

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -90,10 +90,3 @@ func AssertTrue(t *testing.T, got bool) {
 		t.Errorf("got %v, want true", got)
 	}
 }
-
-func AssertFalse(t *testing.T, got bool) {
-	t.Helper()
-	if got {
-		t.Errorf("got %v, want false", got)
-	}
-}
